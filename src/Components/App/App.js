c19e1f7641d6488e57df21f63a36778889146bdf
regy <regy888@gmail.com>
2021-10-15 16:05:14 +0700

    Step 31

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,8 +1,32 @@
 import React from 'react';
 import './App.css';
+
 import SearchBar from '../SearchBar/SearchBar';
+import SearchResult from '../SearchResult/SearchResult';
+import Playlist from '../Playlist/Playlist';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {SearchResults: [{
+      name: 'name1',
+      artist: 'artist1',
+      album: 'album1',
+      id: 'id1'
+    }, {
+      name: 'name2',
+      artist: 'artist2',
+      album: 'album2',
+      id: 'id2'
+    }, {
+      name: 'name3',
+      artist: 'artist3',
+      album: 'album3',
+      id: 'id3'
+    },
+  ]}
+  }
+
   render() {
     return (
       <div>
@@ -10,8 +34,8 @@ class App extends React.Component {
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
-          {/* {<SearchResult />} */}
-          {/* {<Playlist />} */}
+          <SearchResult SearchResults={this.state.SearchResults}/>
+          {<Playlist />}
           </div>
         </div>
       </div>
---
+++ src/Components/App/TrackList/TrackList.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './TrackList.css';
+
+class TrackList extends React.Component {
+    render() {
+        return (
+            <div class="TrackList">
+            </div>
+        )
+    }
+}
+
+export default TrackList;
