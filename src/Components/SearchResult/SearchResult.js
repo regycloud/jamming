b3e602ad3f6640937edd806c3cc850b788455c17
Regy <regy888@gmail.com>
2021-10-16 20:53:49 +0700

    Revert "Step 31"
    This reverts commit c19e1f7641d6488e57df21f63a36778889146bdf.

--- src/Components/SearchResult/SearchResult.js
+++ src/Components/SearchResult/SearchResult.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from 'react';
 import './SearchResult.css';
 import TrackList from '../TrackList/TrackList'
@@ -14,3 +15,6 @@ class SearchResult extends React.Component {
 }
 
 export default SearchResult;
+=======
+import React from 'react'; 
+>>>>>>> parent of c19e1f7 (Step 31)
