ef704e0a4ceb053a7d568e34f731233fa0c6f1c5
Regy <regy888@gmail.com>
2021-10-16 20:50:51 +0700

    Revert "Revert "Step 31""
    This reverts commit 1a4c53ceddab2d7ae73b9fbb3a94edb3b856ba1a.

--- src/Components/SearchResult/SearchResult.js
+++ src/Components/SearchResult/SearchResult.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './SearchResult.css';
+import TrackList from '../TrackList/TrackList'
 
 class SearchResult extends React.Component {
     render() {
         return (
             <div className="SearchResults">
                 <h2>Results</h2>
-                {/* <TrackList /> */}
+                <TrackList tracks={this.props.SearchResult}/>
             </div>
         )
     }
